54dcdcdad8879ab75ab8e4e089964d1947e874b6
Pebaz <pebazium@gmail.com>
2019-12-13 11:12:56 -0500

    Licensing update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ pip install git+https://github.com/Pebaz/nimporter.git
 Please note: In order to use Nimporter, you must have both Nim installed as well
 as the [Nimpy](https://github.com/yglukhov/nimporter) library.
 
-Make sure to star it on GitHub as well while you're up there.
+Make sure to star it on GitHub as well while you're up there. ;)
 """
 
 from setuptools import setup, find_packages
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 setup(
 	name='nimporter',
 	version='0.1.1',
-	license="MPL",
+	license="MIT",
 	description='Compile Nim extensions for Python when imported!',
 	author='http://github.com/Pebaz',
     py_modules=['nimporter']
