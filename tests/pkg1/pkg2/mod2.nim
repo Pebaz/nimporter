e0850b9e91efec1b68f8a123292b604adc5a6229
Pebaz <pebazium@gmail.com>
2020-02-20 16:35:02 -0500

    Added basic unit tests

---
+++ tests/pkg1/pkg2/mod2.nim
@@ -0,0 +1,4 @@
+import nimpy
+
+proc func2(): int {.exportpy.} =
+    return 2
--- tests/test_nimporter.py
+++ tests/test_nimporter.py
@@ -1,4 +1,46 @@
+from setuptools import Extension
 import nimporter
 
-def test_this():
-    assert True
+def test_successful_module_import():
+    "A Nim module can be imported."
+    from pkg1 import mod1
+    assert mod1
+
+def test_successful_nested_module_import():
+    "A Nim module can be imported."
+    from pkg1.pkg2 import mod2
+    assert mod2
+
+def test_build_artifacts():
+    "A hash file, shared library, and __pycache__ is created."
+
+def test_coherent_value():
+    "Python object is returned from Nim function."
+    from pkg1 import mod1
+    assert mod1.func1() == 1
+
+def test_modify_module():
+    "Module is rebuilt when the source file changes."
+    # Print some code to file
+    # Import file
+    # Run file
+    # Change file
+    # Reimport file
+    # Ensure different value returned
+
+def test_hash_changes():
+    "When a module is modified that it's hash does also."
+
+
+def test_build_extension_module():
+    "A Nim module compiles to C properly and that an Extension object is built."
+
+    # assert isinstance(ext, Extension)
+
+
+def test_successful_library_import():
+    "A Nim library can be imported"
+
+
+def test_successful_library_import():
+    "A Nim library can be imported"
