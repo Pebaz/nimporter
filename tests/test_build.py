affc8783c45ec7fe9300c9707be7d4e4f1c7f0bf
Pebaz <pebazium@gmail.com>
2020-03-09 16:49:45 -0400

    tested cd

--- tests/test_build.py
+++ tests/test_build.py
@@ -8,9 +8,35 @@ import nimporter
 from nimporter_cli import clean
 
 
+def test_temp_change_directory():
+    "Test to see if the nimporter.cd function actually works."
+    cwd = Path('.').absolute()
+    temp_dir = Path('nim-temp').absolute()
+    try:
+        temp_dir.mkdir()     
+
+        with nimporter.cd(temp_dir) as tmp:
+            # Yields correct path name
+            assert tmp == temp_dir
+
+            # Actually CDs
+            assert Path().resolve() == temp_dir
+            assert Path().resolve() == tmp
+
+        # CDs back
+        assert Path().resolve() == cwd
+
+    finally:
+        temp_dir.rmdir()
+
+
 def test_custom_build_switches():
     "Test to make sure custom build switches can be used"
 
 
 def test_custom_build_switches_per_platform():
     "Test to make sure that different switches are returned per platform."
+
+
+def test_ignore_cache():
+    pass
