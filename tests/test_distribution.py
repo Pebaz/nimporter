1c47df212d7f54af97cf5ab0cb88e7327bd60c49
Pebaz <pebazium@gmail.com>
2020-03-17 15:41:28 -0400

    built extension module!

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -8,12 +8,6 @@ import nimporter
 from nimporter import Nimporter, NimCompiler
 
 
-def test_build_extension_module():
-    "A Nim module compiles to C properly and that an Extension object is built."
-
-    # assert isinstance(ext, Extension)
-
-
 def test_find_extensions():
     "Make sure that all Nim modules and libraries can be found."
     gold = {
@@ -22,3 +16,31 @@ def test_find_extensions():
     }
     for ext in Nimporter._find_extensions(Path('tests/proj1')):
         assert ext in gold
+
+
+
+def test_build_extension_module():
+    "Make sure A Nim module compiles to C and an Extension object is built."
+    module = Path('tests/proj1/proj1/performance.nim')
+    ext = Nimporter._build_nim_extension(module, Path('tests/proj1'))
+
+    assert isinstance(ext, Extension)
+
+    includes = set(Path(i) for i in ext.include_dirs)
+
+    for source in ext.sources:
+        src = Path(source)
+        assert src.suffix == '.c'
+        assert src.parent in includes
+
+    assert ext.name == 'proj1.performance'
+
+
+def test_build_extension_library():
+    "Make sure a Nim library compiles to C and an Extension object is built."
+
+    # assert isinstance(ext, Extension)
+
+
+def test_build_all_extensions():
+    "Make sure all extensions within a project are compiled correctly."
