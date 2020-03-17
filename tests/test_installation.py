0de1c3555d0edfdd57a740d5380d716f7aefbd88
Pebaz <pebazium@gmail.com>
2020-03-17 16:30:47 -0400

    source and binary distributions!

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -4,17 +4,35 @@ Test to make sure that libraries built with Nimporter can be installed via Pip.
 
 import pytest
 from pathlib import Path
+import subprocess
 import nimporter
 
 
 @pytest.mark.integration_test
 def test_create_sdist():
-    "Test the successful creation of a source distribution"
+    "Test the successful creation of a source distribution."
+    with nimporter.cd('tests/proj1'):
+        subprocess.Popen('python3 setup.py sdist'.split())
+
+        assert Path('dist').exists()
+        assert Path('project1.egg-info').exists()
+        targets = list(Path('dist').glob('project1*'))
+        assert len(targets) == 1
+        assert targets[0].exists()
 
 
 @pytest.mark.integration_test
 def test_create_bdist():
-    pass
+    "Test the successful create of a wheel."
+    with nimporter.cd('tests/proj1'):
+        subprocess.Popen('python3 setup.py bdist_wheel'.split())
+
+        assert Path('dist').exists()
+        assert Path('build').exists()
+        assert Path('project1.egg-info').exists()
+        targets = list(Path('dist').glob('project1*.whl'))
+        assert len(targets) == 1
+        assert targets[0].exists()
 
 
 @pytest.mark.slow_integration_test
