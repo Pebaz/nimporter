b67f28cec95f8be4909016930d0f03489b8e6460
Pebaz <pebazium@gmail.com>
2020-03-17 13:03:43 -0400

    save

--- tests/test_distribution.py
+++ tests/test_distribution.py
@@ -5,6 +5,7 @@ Test to make sure that Nim code can be built and distributed.
 from setuptools import Extension
 from pathlib import Path
 import nimporter
+from nimporter import Nimporter, NimCompiler
 
 
 def test_build_extension_module():
@@ -12,5 +13,12 @@ def test_build_extension_module():
 
     # assert isinstance(ext, Extension)
 
+
 def test_find_extensions():
-    pass
+    "Make sure that all Nim modules and libraries can be found."
+    gold = {
+        Path('tests/proj1/proj1/lib1'),
+        Path('tests/proj1/proj1/performance.nim')
+    }
+    for ext in Nimporter._find_extensions(Path('tests/proj1')):
+        assert ext in gold
