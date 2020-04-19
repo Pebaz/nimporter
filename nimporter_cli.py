3d04195585b50dd7764bc68ccb7bd73f90bc49bd
Pebaz <pebazium@gmail.com>
2020-04-19 13:00:14 -0400

    almost done with bundle cli

--- nimporter_cli.py
+++ nimporter_cli.py
@@ -36,7 +36,7 @@ def clean(dir=pathlib.Path()):
             clean(folder)
 
 
-def main(args=None):
+def main(cli_args=None):
     parser = argparse.ArgumentParser(description='Nimporter CLI')
     subs = parser.add_subparsers(dest='cmd', required=True)
 
@@ -57,7 +57,7 @@ def main(args=None):
     bundle = bundle_parser.add_subparsers(dest='exp', required=True)
     bin_ = bundle.add_parser('bin')
     src = bundle.add_parser('src')
-    args = parser.parse_args(args or sys.argv[1:])
+    args = parser.parse_args(cli_args or sys.argv[1:])
 
     if args.cmd == 'clean':
         cwd = pathlib.Path()
@@ -111,24 +111,45 @@ def main(args=None):
         # 2. If there is not a setup.py, generate a new example one that the
         #    user must modify in order to work (what source file/packages?)
         if not setup.exists():
-            print('No setup.py found in current dir, would you like to generate one?')
-            if input('y/n: '):
-                print('Generating reference setup.py')
+            print('No setup.py found in dir, would you like to generate one?')
+
+            answer = 'a'
+            while answer not in 'YN':
+                answer = input('  Y/N: ').upper() or 'a'
+
+            if answer == 'Y':
                 setup.write_text(
-                    f'import this\n'
-                    f'print("hi")'
+                    f'# Setup.py tutorial:\n'
+                    f'# https://github.com/navdeep-G/setup.py\n'
+                    f'# Edit `packages=` to fit your requirements\n'
+                    f'import setuptools, nimporter\n\n'
+                    f'setuptools.setup(\n'
+                    f'    name={pathlib.Path().name},\n'
+                    f'    packages=[..],  # Please read the above tutorial\n'
+                    f'    ext_modules=nimporter.build_nim_extensions()\n'
+                    f')\n'
                 )
-            else:
-                print('Ok =)')
 
-        PYTHON = 'python' if sys.platform == 'win32' else 'python3'
-            
-        # 3. Depending upon the command, run the appropriate Python command.
-        if args.exp == 'bin':
-            subprocess.Popen(f'{PYTHON} setup.py bdist_wheel'.split()).wait()
+                print('Generated reference setup.py')
+                print('Modify setup.py to point to your modules/packages.')
 
-        elif args.exp == 'src':
-            subprocess.Popen(f'{PYTHON} setup.py sdist'.split()).wait()
+                bundle_type = 'source' if args.exp == 'src' else 'binary'
+
+                print(
+                    f'Once you have finished, run `{" ".join(cli_args)}` again '
+                    f'to create a {bundle_type} distribution package.'
+                )
+        else:
+            pyexe = 'python' if sys.platform == 'win32' else 'python3'
+
+            if args.exp == 'bin':
+                subprocess.Popen(f'{pyexe} setup.py bdist_wheel'.split()).wait()
+
+            elif args.exp == 'src':
+                subprocess.Popen(f'{pyexe} setup.py sdist'.split()).wait()
+
+    return 0
+
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main(sys.argv[1:]))
