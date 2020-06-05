3b73c859006043bc243fcf1c766777df6e9c81d4
philip-h-dye <philip@phd-solutions.com>
2020-06-04 22:17:51 -0400

    tests/proj3/setup.py - remove -w compiler switch

--- tests/proj3/setup.py
+++ tests/proj3/setup.py
@@ -3,13 +3,9 @@ import nimporter
 
 modules = nimporter.build_nim_extensions( exclude_dirs = ['EXCLUDE'] )
 
-for module in modules :
-    module.extra_compile_args.append("-w")
-
 setup(
     name='project3',
     author='https://github.com/Pebaz',
     packages=['proj3'],
-    # ext_modules=nimporter.build_nim_extensions(),
     ext_modules=modules,
 )
