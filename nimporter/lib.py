429eef8983105f3251a9a20e8e579f36ca931b4a
Sekou Diao <diao.sekou.nlp@gmail.com>
2023-01-20 10:33:54 +0100

    Update type annotations

--- nimporter/lib.py
+++ nimporter/lib.py
@@ -167,7 +167,7 @@ def run_process(
 
 
 @contextmanager
-def cd(path: Union[Path, str]) -> Iterator[Union[Path, str]]:
+def cd(path: Union[Path, str]) -> Iterator[Path]:
     "Convenience function to step in and out of a directory temporarily."
     cwd = os.getcwd()
     os.chdir(path)
