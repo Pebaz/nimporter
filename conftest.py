23b462550c58c74ae1083cc533a5b912faed2ff8
Pebaz <pebazium@gmail.com>
2023-07-28 16:49:32 -0700

    Testing out new way of uninstalling nimporter

--- conftest.py
+++ conftest.py
@@ -3,13 +3,32 @@ Pytest hook to ensure that no build artifacts are leftover from previous runs.
 See the tests/README.md file for more info.
 """
 
+import os
+import sys
+import shlex
+import pathlib
+import pytest
+
 # Hilariously, this has a pretty important role in the testing of this project.
 # Without this line, deeply nested tests importing the local development copy
 # of `nimporter` would fail without some PYTHONPATH manipulation. Better to
 # just import and cache it in `sys.modules` at the project root. ;)
-import pathlib
 from nimporter.cli import nimporter_clean
+from nimporter.lib import run_process, PYTHON
 
 
 def pytest_sessionstart(session):
     nimporter_clean(pathlib.Path())
+
+
+def pytest_sessionfinish(session: pytest.Session, exitstatus: int) -> None:
+    try:
+        code, _, _ = run_process(
+            shlex.split(f'{PYTHON} -m pip uninstall nimporter -y'),
+            'NIMPORTER_INSTRUMENT' in os.environ
+        )
+
+        sys.modules.pop('nimporter', None)
+    except Exception as e:
+        print('Failed to uninstall Nimporter after all tests completed:', e)
+        raise
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,44 @@
+"""
+! See conftest.py for `pytest_sessionfinish` hook to uninstall Nimporter.
+"""
+
 import os
 import sys
 import time
 import shlex
-import contextlib
+import pytest
+import pathlib
+
+# * It's actually pretty important that the local version of Nimporter is used
+# * to run tests.
+
+# current_path = pathlib.Path().expanduser().resolve().absolute()
+# try:
+#     os.chdir('..')
+#     import nimporter
+
+#     project_root = (pathlib.Path(__file__)
+#         .expanduser()
+#         .resolve()
+#         .absolute()
+#         .parent
+#         .parent
+#     )
+#     nimporter_path = pathlib.Path(nimporter.__file__)
+
+#     assert nimporter_path.is_relative_to(project_root), (
+#         f'Accidentally imported system version of Nimporter: '
+#         f'{nimporter.__file__} != {project_root}'
+#     )
+# except ImportError:
+#     "Expected case"
+# finally:
+#     os.chdir(current_path)
+
 from nimporter.lib import run_process, PYTHON
 
 
-@contextlib.contextmanager
+@pytest.fixture(scope='session')
 def temporarily_install_nimporter():
     # Hopefully prevent permission denied errors in CI when deleting artifacts
     time.sleep(5)
@@ -20,12 +52,6 @@ def temporarily_install_nimporter():
         assert code == 0, (
             f'Nimporter failed to install:\nstdout: {out}\nstderr: {err}'
         )
-
-        yield
-    finally:
-        code, _, _ = run_process(
-            shlex.split(f'{PYTHON} -m pip uninstall nimporter -y'),
-            'NIMPORTER_INSTRUMENT' in os.environ
-        )
-
-        sys.modules.pop('nimporter', None)
+    except Exception as e:
+        print('Failed to install Nimporter:', e)
+        raise
